67c6c5c975d7b4429d4fe295c46bcd2e577d348b
xrotwang <xrotwang@googlemail.com>
2019-02-02 11:21:47 +0100

    experimental implementation for https://github.com/clld/concepticon-data/issues/408

--- src/pyconcepticon/commands.py
+++ src/pyconcepticon/commands.py
@@ -1,4 +1,5 @@
 import os
+import re
 from collections import Counter, defaultdict
 import operator
 from functools import partial
@@ -101,6 +102,21 @@ def link(args):
             raise ParserError('no file %s found' % args.args[0])
 
     rewrite(conceptlist, Linker(conceptlist.stem, api.conceptsets.values()))
+
+
+@command()
+def notlinked(args):
+    api = Concepticon(args.repos)
+    i = 0
+    for _, cl in sorted(api.conceptlists.items(), key=lambda p: p[0]):
+        for concept in sorted(cl.concepts.values(), key=lambda p: int(re.match('([0-9]+)', p.number).groups()[0])):
+            if not concept.concepticon_id:
+                candidates = [c for c in list(api.lookup([concept.label]))[0] if c[3] < 3]
+                if candidates:
+                    candidate = sorted(candidates, key=lambda c: c[3])[0]
+                    candidate = '{0} [{1}]'.format(candidate[2], candidate[1])
+                    i += 1
+                    print('{0} {1.id}: {1.label}: {2}'.format(i, concept, candidate))
 
 
 @command()
