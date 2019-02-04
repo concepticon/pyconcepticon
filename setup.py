d5f12f67dc11a6f8a5c38c580573a8763e3d0975
xrotwang <xrotwang@googlemail.com>
2019-02-04 15:17:13 +0100

    release 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyconcepticon',
-    version='2.0.1.dev0',
+    version='2.0.1',
     description='programmatic access to concepticon-data',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
--- src/pyconcepticon/__init__.py
+++ src/pyconcepticon/__init__.py
@@ -1,5 +1,5 @@
 # noqa
 from pyconcepticon.api import Concepticon
 
-__version__ = "2.0.1.dev0"
+__version__ = "2.0.1"
 
