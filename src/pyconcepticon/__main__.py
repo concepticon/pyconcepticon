b788c559b11710190e468729125df2b68f57c80d
lingulist <mattis.list@lingpy.org>
2019-11-21 10:57:34 +0100

    remove whitespaces

--- src/pyconcepticon/__main__.py
+++ src/pyconcepticon/__main__.py
@@ -1,10 +1,13 @@
 """
 Main command line interface of the pyconcepticon package.
+
 Like programs such as git, this cli splits its functionality into sub-commands
 (see e.g. https://docs.python.org/2/library/argparse.html#sub-commands).
 The rationale behind this is that while a lot of different tasks may be triggered using
 this cli, most of them require common configuration.
+
 The basic invocation looks like
+
     concepticon [OPTIONS] <command> [args]
 """
 import sys
