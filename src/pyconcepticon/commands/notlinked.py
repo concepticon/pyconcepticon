f631ad3196eb316e27967a59e807b4a5c478141c
Robert Forkel <xrotwang@googlemail.com>
2021-05-28 13:29:55 +0200

    enhanced notlinked command

--- src/pyconcepticon/commands/notlinked.py
+++ src/pyconcepticon/commands/notlinked.py
@@ -9,24 +9,35 @@ def register(parser):
         '--full',
         action='store_true',
         default=False,
-        help='Compare not-linked concepts to all concept sets - rather than just new ones.',
+        help='',
+    )
+    parser.add_argument(
+        '--inid',
+        default=False,
+        help="substring in list ID for lists to consider - leave empty to consider all.",
+    )
+    parser.add_argument(
+        '--similarity-threshold',
+        type=int,
+        default=2,
+        help='Maximal (dis)similarity index to still regard as potential match',
     )
 
 
 def run(args):
-    i = 0
+    i, notlinked = 0, []
     for _, cl in sorted(args.repos.conceptlists.items(), key=lambda p: p[0]):
-        mincsid = None if args.full else max(
-            [int(c.concepticon_id) for c in cl.concepts.values() if c.concepticon_id])
-        for concept in sorted(
-                cl.concepts.values(),
-                key=lambda p: int(re.match('([0-9]+)', p.number).groups()[0])):
-            if not concept.concepticon_id:
-                candidates = [
-                    c for c in list(args.repos.lookup([concept.label], mincsid=mincsid))[0]
-                    if c[3] < 3]
-                if candidates:
-                    candidate = sorted(candidates, key=lambda c: c[3])[0]
-                    candidate = "{0} [{1}]".format(candidate[2], candidate[1])
-                    i += 1
-                    print("{0} {1.id}: {1.label}: {2}".format(i, concept, candidate))
+        if (not args.inid) or args.inid in cl.id:
+            for concept in sorted(
+                    cl.concepts.values(),
+                    key=lambda p: int(re.match('([0-9]+)', p.number).groups()[0])):
+                if not concept.concepticon_id:
+                    notlinked.append(concept)
+    for j, matches in enumerate(args.repos.lookup(
+            [c.label for c in notlinked], full_search=not args.full)):
+        if matches:
+            candidates = sorted(matches, key=lambda x: x[-1])
+            cid, cgl = candidates[0][2:4]
+            if cgl <= args.similarity_threshold:
+                i += 1
+                print('{0} {1.id}: {1.label}: {2} [{3}]'.format(i, notlinked[j], cid, cgl))
