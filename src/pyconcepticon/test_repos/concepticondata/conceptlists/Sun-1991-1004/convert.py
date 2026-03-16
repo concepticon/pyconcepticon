668235089a9a1a459257af60ffdc0a59bf353e62
Robert Forkel <xrotwang@googlemail.com>
2026-03-16 11:19:02 +0100

    linting

--- src/pyconcepticon/test_repos/concepticondata/conceptlists/Sun-1991-1004/convert.py
+++ src/pyconcepticon/test_repos/concepticondata/conceptlists/Sun-1991-1004/convert.py
@@ -1,7 +1,10 @@
+"""
+test
+"""
 import pathlib
 
 d = pathlib.Path(__file__).resolve().parent
-t = d.parent.joinpath('{}.tsv'.format(d.name)).read_text(encoding='utf8')
+t = d.parent.joinpath(f'{d.name}.tsv').read_text(encoding='utf8')
 t = t.replace(
     '[{"ID":"Sun-1991-1004-83","DEGREE":7}]', '[{"ID":"Sun-1991-1004-87","DEGREE":7}]')
-d.joinpath('{}.tsv'.format(d.name)).write_text(t, encoding='utf8')
+d.joinpath(f'{d.name}.tsv').write_text(t, encoding='utf8')
